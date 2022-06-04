2cc72f4b3e776a97090a96284f6c1897a21826bd
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-06-04 10:07:01 +0300

    adding properties to the model images

--- insta/models.py
+++ insta/models.py
@@ -3,3 +3,5 @@ from django.db import models
 # Create your models here.
 class Image(models.Model):
     image=models.ImageField(upload_to = 'images/', default='no image')
+    title = models.CharField(max_length=100, default='SOME STRING')
+    caption = models.TextField(max_length=300, null=True)
