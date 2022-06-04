19362de27497f0f3ca883ec0f93d4f0effdcb463
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-06-04 14:04:53 +0300

    creating a registration form

--- users/forms.py
+++ users/forms.py
@@ -3,3 +3,10 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from.models import *
 
+class RegistrationForm(UserCreationForm):
+    username = forms.CharField(max_length=50)
+    email=forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ('username','email','password1','password2')
