e0a6801ea3c921867ab2e9e9e5d3b844df8186ed
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-06-04 14:08:02 +0300

    creating a profile update form

--- users/forms.py
+++ users/forms.py
@@ -18,3 +18,9 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username','email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields =['image', 'bio']
+
